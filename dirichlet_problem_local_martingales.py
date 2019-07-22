29e73dfd74d0418f4802592fbe031a25725166de
William Underwood <willunderwood13@btinternet.com>
2019-07-21 20:05:52 -0400

    started bm simulation

--- dirichlet_problem_local_martingales.py
+++ dirichlet_problem_local_martingales.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
+
 # draw a region
 
 def get_circle(center, radius, num_samples):
@@ -77,23 +78,28 @@ def data_to_polygon(dU, boundary_values, ref_height, color, alpha):
 def plot_region_and_boundary_condition(dU, boundary_values, num_samples):
 	
 	# set up plot
-	fig = plt.figure(figsize=(10,6))
+	fig = plt.figure(figsize=(5,3))
 	ax = fig.add_subplot(111, projection='3d')
 	
 	# region
 	ax.add_collection3d(plt.fill_between(dU[:,0], dU[:,1], 0, color='lightsteelblue', linewidth=0))
 	
-	# region boundary
-	ax.plot(xs=dU[0:num_samples,0], ys=dU[0:num_samples,1], zs=0, color='slateblue', zorder=10, linewidth=2)
-	ax.plot(xs=dU[num_samples:,0], ys=dU[num_samples:,1], zs=0, color='slateblue', zorder=10, linewidth=2)
-	
 	# vertical shading
 	ax.add_collection3d(data_to_polygon(dU[0:num_samples,:], boundary_values[0:num_samples], 0, 'r', 0.5))
 	ax.add_collection3d(data_to_polygon(dU[num_samples:,:], boundary_values[num_samples:,:], 0, 'r', 0.5))
 	
+	# region boundary
+	ax.plot(xs=dU[0:num_samples,0], ys=dU[0:num_samples,1], zs=0, color='slateblue', linewidth=2, zorder=4)
+	ax.plot(xs=dU[num_samples:,0], ys=dU[num_samples:,1], zs=0, color='slateblue', linewidth=2, zorder=4)
+	
 	# phi values
-	ax.plot(xs=dU[0:num_samples,0], ys=dU[0:num_samples,1], zs=boundary_values[0:num_samples,0], color='r', zorder=10, linewidth=2)
-	ax.plot(xs=dU[num_samples:,0], ys=dU[num_samples:,1], zs=boundary_values[num_samples:,0], color='r', zorder=10, linewidth=2)
+	ax.plot(xs=dU[0:num_samples,0], ys=dU[0:num_samples,1], zs=boundary_values[0:num_samples,0], color='r', linewidth=2, zorder=5)
+	ax.plot(xs=dU[num_samples:,0], ys=dU[num_samples:,1], zs=boundary_values[num_samples:,0], color='r', linewidth=2, zorder=5)
+	
+	# text
+	plt.text(x=3.8, y=0, z=0, s='$U$', fontsize=20, zorder=6)
+	plt.text(x=2, y=-5.7, z=0, s='$\partial U$', fontsize=20, zorder=6)
+	plt.text(x=-3, y=7.5, z=0, s='$\phi(\partial U)$', fontsize=20, zorder=6)
 	
 	# axis limits
 	ax.set_xlim([-4,6])
@@ -113,24 +119,73 @@ def plot_region_and_boundary_condition(dU, boundary_values, num_samples):
 
 # simulate one-dimensional BM
 
-def sim_bm(T, mesh_size):
+def sim_bm(x, T, num_samples):
 
-    num_samples = int(T/mesh_size)
-    sd = np.sqrt(mesh_size)
+	mesh_size = T/num_samples
+	sd = np.sqrt(mesh_size)
 
-    normal_vec = np.random.normal(loc=0, scale=sd, size=num_samples)
-    bm = np.cumsum(normal_vec)
-
-    return bm
+	normal_vec = np.random.normal(loc=0, scale=sd, size=num_samples)
+	normal_vec[0] = normal_vec[0] + x
+	bm = np.cumsum(normal_vec)
+	return bm
 
 
 # simulate two-dimensional BM
 
-def sim_2d_bm(T, mesh_size):
+def sim_2d_bm(xy, T, num_samples):
     
-    x_bm = sim_bm(T, mesh_size)
-    y_bm = sim_bm(T, mesh_size)
+    x_bm = sim_bm(xy[0], T, num_samples)
+    y_bm = sim_bm(xy[1], T, num_samples)
     
-    bm_2 = np.array([x_bm, y_bm])
+    bm_2 = np.array([x_bm, y_bm]).T
 
     return bm_2
+	
+
+def inside_U(xy):
+
+	x = xy[0]
+	y = xy[1]
+
+	if (x-1)**2 + y**2 >= 25:
+		return False
+	elif x**2 + y**2 <= 4:
+		return False
+	else:
+		return True
+
+	
+
+def up_to_escape(bm_2d):
+
+	still_in_U = np.apply_along_axis(inside_U, 1, bm_2d)
+	
+	last_time_before_escape = list(still_in_U).index(False)
+	bm_up_to_escape = bm_2d[0:(last_time_before_escape + 1)]
+	
+	return bm_up_to_escape
+	
+	
+def plot_single_bm_path(dU, T, num_samples):
+
+	bm_2 = sim_2d_bm((3.5,0), T=10, num_samples=100)
+	
+	fig = plt.figure(figsize=(10,10))
+	ax = fig.add_subplot(111)
+	
+	# region
+	plt.fill_between(dU[:,0], dU[:,1], linewidth=0, color='lightsteelblue')	
+	
+	# boundary
+	ax.plot(dU[0:num_samples,0], dU[0:num_samples,1], color='slateblue', linewidth=2, zorder=2)
+	ax.plot(dU[num_samples:,0], dU[num_samples:,1], color='slateblue', linewidth=2, zorder=2)
+	
+	# bm
+	B = sim_2d_bm((3,0), T, 10000)
+	B = up_to_escape(B)
+	ax.plot(B[:,0],B[:,1], linewidth=0.5)
+
+	
+	plt.show()
+
+
